201f0b270e1383aff5249cad66fc19bdce4ffb39
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-11-26 00:44:06 +0100

    Update bike_util.py

--- pipeline/exp/bike_util.py
+++ pipeline/exp/bike_util.py
@@ -1,42 +1,103 @@
 from typing import List
 
+import absl
 import tensorflow as tf
-import tensorflow_model_analysis as tfma
+from tensorflow import keras
 import tensorflow_transform as tft
-from tensorflow_transform.tf_metadata import schema_utils
+
 from tfx.components.trainer.fn_args_utils import DataAccessor
+from tfx.components.trainer.fn_args_utils import FnArgs
 from tfx_bsl.tfxio import dataset_options
 
-# Categorical features are assumed to each have a maximum value in the dataset.
-_MAX_CATEGORICAL_FEATURE_VALUES = [24, 31, 12]
+_FEATURE_KEY = 'text'
+_LABEL_KEY = 'label'
 
-_CATEGORICAL_FEATURE_KEYS = [
-    'trip_start_hour', 'trip_start_day', 'trip_start_month',
-    'pickup_census_tract', 'dropoff_census_tract', 'pickup_community_area',
-    'dropoff_community_area'
-]
+# There are 100 entries in the imdb_small dataset. ExampleGen splits the dataset
+# with a 2:1 train-eval ratio. Batch_size is an empirically sound
+# configuration.
+# To train on the entire imdb dataset, please refer to imdb_dataset_utils.py
+# and change the batch configuration accordingly.
+_DROPOUT_RATE = 0.2
+_EMBEDDING_UNITS = 64
+_EVAL_BATCH_SIZE = 5
+_HIDDEN_UNITS = 64
+_LEARNING_RATE = 1e-4
+_LSTM_UNITS = 64
+_VOCAB_SIZE = 8000
+_MAX_LEN = 400
+_TRAIN_BATCH_SIZE = 10
 
-_DENSE_FLOAT_FEATURE_KEYS = ['trip_miles', 'fare', 'trip_seconds']
 
-# Number of buckets used by tf.transform for encoding each feature.
-_FEATURE_BUCKET_COUNT = 10
+def _transformed_name(key):
+  return key + '_xf'
 
-_BUCKET_FEATURE_KEYS = [
-    'pickup_latitude', 'pickup_longitude', 'dropoff_latitude',
-    'dropoff_longitude'
-]
 
-# Number of vocabulary terms used for encoding VOCAB_FEATURES by tf.transform
-_VOCAB_SIZE = 1000
+def _transformed_names(keys):
+  return [_transformed_name(key) for key in keys]
 
-# Count of out-of-vocab buckets in which unrecognized VOCAB_FEATURES are hashed.
-_OOV_SIZE = 10
 
-_VOCAB_FEATURE_KEYS = [
-    'payment_type',
-    'company',
-]
+# Tf.Transform considers these features as "raw"
+def _get_raw_feature_spec(schema):
+  return schema_utils.schema_as_feature_spec(schema).feature_spec
 
-# Keys
-_LABEL_KEY = 'tips'
-_FARE_KEY = 'fare'
+
+
+def _fill_in_missing(x):
+  """Replace missing values in a SparseTensor.
+  Fills in missing values of `x` with '' or 0, and converts to a dense tensor.
+  Args:
+    x: A `SparseTensor` of rank 2.  Its dense shape should have size at most 1
+      in the second dimension.
+  Returns:
+    A rank 1 tensor where missing values of `x` have been filled in.
+  """
+  if not isinstance(x, tf.sparse.SparseTensor):
+    return x
+
+  default_value = '' if x.dtype == tf.string else 0
+  return tf.squeeze(
+      tf.sparse.to_dense(
+          tf.SparseTensor(x.indices, x.values, [x.dense_shape[0], 1]),
+          default_value),
+      axis=1)
+
+def preprocessing_fn(inputs):
+  """tf.transform's callback function for preprocessing inputs.
+  Args:
+    inputs: map from feature keys to raw not-yet-transformed features.
+  Returns:
+    Map from string feature key to transformed feature operations.
+  """
+  outputs = {}
+  for key in _DENSE_FLOAT_FEATURE_KEYS:
+    # If sparse make it dense, setting nan's to 0 or '', and apply zscore.
+    outputs[_transformed_name(key)] = tft.scale_to_z_score(
+        _fill_in_missing(inputs[key]))
+
+  for key in _VOCAB_FEATURE_KEYS:
+    # Build a vocabulary for this feature.
+    outputs[_transformed_name(key)] = tft.compute_and_apply_vocabulary(
+        _fill_in_missing(inputs[key]),
+        top_k=_VOCAB_SIZE,
+        num_oov_buckets=_OOV_SIZE)
+
+  for key in _BUCKET_FEATURE_KEYS:
+    outputs[_transformed_name(key)] = tft.bucketize(
+        _fill_in_missing(inputs[key]), _FEATURE_BUCKET_COUNT)
+
+  for key in _CATEGORICAL_FEATURE_KEYS:
+    outputs[_transformed_name(key)] = _fill_in_missing(inputs[key])
+
+  # Was this passenger a big tipper?
+  taxi_fare = _fill_in_missing(inputs[_FARE_KEY])
+  tips = _fill_in_missing(inputs[_LABEL_KEY])
+  outputs[_transformed_name(_LABEL_KEY)] = tf.compat.v1.where(
+      tf.math.is_nan(taxi_fare),
+      tf.cast(tf.zeros_like(taxi_fare), tf.int64),
+      # Test if the tip was > 20% of the fare.
+      tf.cast(
+          tf.greater(tips, tf.multiply(taxi_fare, tf.constant(0.2))), tf.int64))
+
+  return outputs
+
+
